1ad399edc3d57a14cfd72bfa341313f4b2b9f3fa
GiovannyS <GiovannySalotto@gmail.com>
2021-02-18 21:10:19 -0400

    Editar palabra "Clima" por "Resultados"

--- Medinet/Scripts/AnalyticalReport.js
+++ Medinet/Scripts/AnalyticalReport.js
@@ -338,16 +338,17 @@ $(document).ready(function () {
 
         //------------------------------------------------------------//
 
-        //const options = {
-        //     'jsPDF': {
-        //         'orientation': 'p',
-        //         'unit': 'mm',
-        //         'format': 'letter',
-        //     }
-        // };
-        //const jsPdfWrapper = new JsPdfWrapper(options);
-        //const targetElement = $('#contenido-sistema')[0];
-        //jsPdfWrapper.DownloadFromHTML(targetElement, pdfFileName);
+        const options = {
+             'jsPDF': {
+                 'orientation': 'p',
+                 'unit': 'pt',
+                'format': 'letter',
+                 'precision':2,
+             }
+         };
+        const jsPdfWrapper = new JsPdfWrapper(options);
+        const targetElement = $('#contenido-sistema')[0];
+        jsPdfWrapper.DownloadFromHTML(targetElement, pdfFileName);
 
         //-----------------------------------------------------------//
 
@@ -369,6 +370,51 @@ $(document).ready(function () {
         //    'elementHandlers': specialElementHandlers
         //});
         //pdf.save(pdf_name);
+
+        //------------------------------------------------------------//
+
+        //var pdf = new jsPDF('p', 'pt', 'letter')
+
+        //    // source can be HTML-formatted string, or a reference
+        //    // to an actual DOM element from which the text will be scraped.
+        //    , source = $('#contenido-sistema')[0]
+
+        //    // we support special element handlers. Register them with jQuery-style
+        //    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+        //    // There is no support for any other type of selectors
+        //    // (class, of compound) at this time.
+        //    , specialElementHandlers = {
+        //        // element with id of "bypass" - jQuery style selector
+        //        '#bypassme': function (element, renderer) {
+        //            // true = "handled elsewhere, bypass text extraction"
+        //            return true
+        //        }
+        //    }
+
+        //margins = {
+        //    top: 40,
+        //    bottom: 30,
+        //    left: 20,
+        //    width: 250
+        //};
+        //// all coords and widths are in jsPDF instance's declared units
+        //// 'inches' in this case
+        //pdf.fromHTML
+        //    (
+        //        source // HTML string or DOM elem ref.
+        //        , margins.left // x coord
+        //        , margins.top // y coord
+        //        , {
+        //            'width': margins.width // max width of content on PDF
+        //            , 'elementHandlers': specialElementHandlers
+        //        }
+        //        , function (dispose) {
+        //            // dispose: object with X, Y of the last line add to the PDF
+        //            // this allow the insertion of new lines after html
+        //            pdf.save('Mypdf.pdf');
+        //        }
+        //        , margins
+        //)
         return false;
     });
 });
